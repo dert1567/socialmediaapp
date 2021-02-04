15c9ddce90e38bd1f1790bd140c17b880192ff50
“Daniel <daniel.ragimov@web.de>
2021-02-04 01:14:33 +0100

    signup

---
+++ functions/dbschema.js
@@ -0,0 +1,17 @@
+
+
+let db = {
+
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: "2021-01-31T23:50:40.582Z",
+            likeCount: 5,
+            commentCount: 2
+            
+        }
+    ]
+
+
+}
