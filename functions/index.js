9ed94db6189dece5443671ed7e3f2a743898aa46
dert1567 <35528731+dert1567@users.noreply.github.com>
2021-02-04 01:15:32 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -10,12 +10,7 @@ const app = express()
 
 
 var config = {
-    apiKey: "AIzaSyBKdqHmeQuEQqXFygppTwQfpWTuEY5ky8o",
-    authDomain: "socialmediaapp-8a9d8.firebaseapp.com",
-    projectId: "socialmediaapp-8a9d8",
-    storageBucket: "socialmediaapp-8a9d8.appspot.com",
-    messagingSenderId: "28066724904",
-    appId: "1:28066724904:web:1b3eefcb732807c73fc91d"
+ 
 };
 
 firebase.initializeApp(config)
