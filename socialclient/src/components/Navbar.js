7c928bdb6bea08fe4b670d30c06f180a90b7b9fd
“Daniel <daniel.ragimov@web.de>
2021-02-09 01:05:08 +0100

    screna getting from server

---
+++ socialclient/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import Link from 'react-router-dom/Link'
+
+
+import AppBar from '@material-ui/core/AppBar'
+import { Button, Toolbar } from '@material-ui/core'
+
+export class Navbar extends Component {
+    render() {
+        return (
+            <AppBar>
+                <Toolbar className="nav-container">
+                    <Button color="inherit" component={Link} to="/login">Login</Button>
+                    <Button color="inherit" component={Link} to="/">Home</Button>
+                    <Button color="inherit" component={Link} to="/signup">signup</Button>
+                </Toolbar>
+            </AppBar>
+        )
+    }
+}
+
+export default Navbar
